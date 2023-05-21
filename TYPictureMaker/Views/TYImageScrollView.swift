42d5959dff06d881f9a2daaedfe9401616df06d0
Sheldon <tinarychina@gmail.com>
2023-05-21 15:57:21 +0800

    调整图片缩放

--- TYPictureMaker/Views/TYImageScrollView.swift
+++ TYPictureMaker/Views/TYImageScrollView.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class TYImageScrollView : UIScrollView {
+class TYImageScrollView : UIView {
     
     var image: UIImage {
         didSet {
@@ -15,9 +15,20 @@ class TYImageScrollView : UIScrollView {
         }
     }
     
+    private lazy var scrollView: UIScrollView = {
+        let sv = UIScrollView(frame: bounds)
+        sv.showsHorizontalScrollIndicator = false
+        sv.showsVerticalScrollIndicator = false
+        sv.delegate = self
+        sv.minimumZoomScale = 1
+        sv.maximumZoomScale = 2
+        addSubview(sv)
+        return sv
+    }()
+    
     private lazy var imageView : UIImageView = {
         let imageView = UIImageView(frame: .zero)
-        addSubview(imageView)
+        scrollView.addSubview(imageView)
         return imageView
     }()
     
@@ -26,22 +37,38 @@ class TYImageScrollView : UIScrollView {
         super.init(frame: .zero)
 //        imageView.image = image
         backgroundColor = .white
-        showsHorizontalScrollIndicator = false
-        showsVerticalScrollIndicator = false
+        
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // 通过图片的size得到视图的size
+    func getImageViewSizeWith(image:UIImage) -> CGSize {
+        var scrollW = bounds.size.width + 1
+        var scrollH = scrollW / image.size.width * image.size.height
+        if (scrollH < bounds.size.height) {
+            scrollH = bounds.size.height + 1
+            scrollW = scrollH / image.size.height * image.size.width
+        }
+        return CGSize(width: scrollW, height: scrollH)
+    }
+    
     override func layoutSubviews() {
         super.layoutSubviews()
-//        imageView.frame = CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height)
-        imageView.frame = bounds
-        contentSize = bounds.size
+        scrollView.frame = bounds
+        scrollView.contentSize = getImageViewSizeWith(image: image)
+        imageView.frame = CGRect(origin: CGPoint(x: 0, y: 0), size: scrollView.contentSize)
     }
     
     // 双指缩放功能
     
     // 双击显示正常功能
+}
+
+extension TYImageScrollView: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
 }
