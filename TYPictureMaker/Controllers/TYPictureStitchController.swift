49377ee2ef55726bb7307e1c8c86f10991943a80
Sheldon <tinarychina@gmail.com>
2023-05-23 20:04:59 +0800

    将图片显示视图替换为UIStackView

--- TYPictureMaker/Controllers/TYPictureStitchController.swift
+++ TYPictureMaker/Controllers/TYPictureStitchController.swift
@@ -80,7 +80,7 @@ class TYPictureStitchController: UIViewController {
     }()
     
     // 图片展示视图
-    private var updownView :TYVerticalLayoutView?
+    private var updownView :TYNormalLayoutView?
     
     private var heightConstraint : Constraint?
     
@@ -133,7 +133,8 @@ class TYPictureStitchController: UIViewController {
     }
     
     func setupSubViews() {
-        let updownView = TYVerticalLayoutView(topImage: images.first!, bottomImage: images.last!)
+        let updownView = TYNormalLayoutView(images: images)
+        updownView.axis = .horizontal
         updownView.pandding = CGFloat(editInfo.borderCorner.pictureBorder)
         updownView.imagePandding = CGFloat(editInfo.borderCorner.imageBorder)
         updownView.imageCornerRadio = CGFloat(editInfo.borderCorner.imageCornerRadio)
---
+++ TYPictureMaker/Views/TYNormalLayoutView.swift
@@ -0,0 +1,87 @@
+//
+//  TYUpdownView.swift
+//  TYPictureMaker
+//
+//  Created by Sheldon Tian on 2023/5/17.
+//  正常垂直布局
+
+import UIKit
+import SnapKit
+
+class TYNormalLayoutView: UIView {
+    var images : [UIImage]
+    
+    var axis : NSLayoutConstraint.Axis = .vertical {
+        didSet {
+            stackView.axis = axis
+        }
+    }
+    
+    private lazy var stackView : UIStackView = {
+        let imageViews : [TYImageCollectView] = images.map { image in
+            TYImageCollectView(with: image)
+        }
+        let stackView = UIStackView(arrangedSubviews: imageViews)
+        stackView.axis = axis
+        stackView.alignment = .fill
+        stackView.distribution = .fillEqually
+        addSubview(stackView)
+        return stackView
+    }()
+    
+    var pandding : CGFloat = 0 {
+        didSet {
+            update(pandding: pandding)
+        }
+    }
+    
+    var imagePandding: CGFloat = 0 {
+        didSet {
+            updateSubView(pandding: imagePandding)
+        }
+    }
+    
+    var imageCornerRadio : CGFloat = 0 {
+        didSet {
+            updateSubView(cornerRadio: imageCornerRadio)
+        }
+    }
+    
+    init(images: [UIImage]) {
+        self.images = images
+        super.init(frame: .zero)
+        setupSubViews()
+        backgroundColor = .white
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupSubViews() {
+        stackView.snp.makeConstraints { make in
+            make.edges.equalToSuperview().inset(UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0))
+        }
+    }
+    
+    func update(pandding: CGFloat) {
+        stackView.snp.remakeConstraints { make in
+            make.edges.equalToSuperview().inset(UIEdgeInsets(top: pandding, left: pandding, bottom: pandding, right: pandding))
+        }
+        
+    }
+    
+    func updateSubView(pandding: CGFloat) {
+        stackView.spacing = pandding
+    }
+    
+    func updateSubView(cornerRadio: CGFloat) {
+        stackView.arrangedSubviews.forEach { view in
+            if (view is TYImageCollectView) {
+                let imgView = view as! TYImageCollectView
+                imgView.cornerRaido = cornerRadio
+            }
+        }
+    }
+}
