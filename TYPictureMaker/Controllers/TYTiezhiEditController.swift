dcf051102618fa0c7ced4b8793806786b661bb7f
Sheldon <tinarychina@gmail.com>
2023-05-26 16:40:48 +0800

    添加贴纸资源

---
+++ TYPictureMaker/Controllers/TYTiezhiEditController.swift
@@ -0,0 +1,21 @@
+//
+//  TYTiezhiEditController.swift
+//  TYPictureMaker
+//
+//  Created by Sheldon Tian on 2023/5/26.
+//
+
+import UIKit
+
+class TYTiezhiEditController : TYOprationEditController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func setupSubviews() {
+        super.setupSubviews()
+    }
+}
---
+++ TYPictureMaker/Views/TYPanView.swift
@@ -0,0 +1,8 @@
+//
+//  TYPanView.swift
+//  TYPictureMaker
+//
+//  Created by Sheldon Tian on 2023/5/26.
+//  可拖动的视图
+
+import Foundation
